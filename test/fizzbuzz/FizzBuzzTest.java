d20a09bca321002551dcf5682f5dd3e0319046d2
Diam-I <amina.didane@univ-tlse3.fr>
2024-04-12 15:01:26 +0200

    0

---
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+package fizzbuzz;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+public class FizzBuzzTest {
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+}
