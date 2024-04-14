04ea3bfde80321ac49605ab51c25cb343567fb7f
Diam-I <amina.didane@univ-tlse3.fr>
2024-04-14 19:29:05 +0200

    8

--- testBowling/src/tdd/Game.java
+++ testBowling/src/tdd/Game.java
@@ -3,19 +3,37 @@ package tdd;
 public class Game {
 	int score = 0 ; 
 	int nbLancers = 0 ; 
+	int nbBonus = 0 ; 
 	
 	void roll (int i) {
-		nbLancers ++ ; 
+		nbLancers ++ ;
+		
+
+		if (nbBonus > 0) {
+			score += i ; 
+			nbBonus -- ;
+		}
+		if (nbLancers == 1 && i == 10) {
+			nbBonus = 2 ;
+		}
+		
 		if (nbLancers == 3 && score == 10) {
 			score += 2*i ;
 		}
 		else {
 			score += i ;
 		}
+		
+		
+		
+		
+		
+		
 
 	}
 	
 	int score () {
+		
 		
 		return score ;
 	}
