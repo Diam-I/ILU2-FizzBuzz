93817e8cae65b4cd8dc5682ef603d3cd1053fe71
Diam-I <amina.didane@univ-tlse3.fr>
2024-04-14 17:59:28 +0200

    2

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,7 +2,23 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	
-	public static Object fizzbuzz(int i) {
-		return "1";
+	static int[] valeursSpeciales = {3,5} ;
+	static String[] motsSpeciaux = {"fizz","buzz"};
+	
+	public static String fizzbuzz(int entree) {
+		StringBuilder resultat = new StringBuilder();
+		
+		for (int i=0; i<valeursSpeciales.length;i++) {
+			if (entree%valeursSpeciales[i] == 0) {
+				resultat.append(motsSpeciaux[i]);
+			}
+		}
+		if (resultat.length()==0) {
+			return Integer.toString(entree);
+		}
+		else {
+			return resultat.toString();
+		}
+		
 	}
 }
