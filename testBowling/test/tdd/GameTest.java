9c4cc4b9b11b16b41e2bd3c564ba9f965b1d61ca
Diam-I <amina.didane@univ-tlse3.fr>
2024-04-14 18:38:41 +0200

    6

--- testBowling/test/tdd/GameTest.java
+++ testBowling/test/tdd/GameTest.java
@@ -33,6 +33,16 @@ public class GameTest {
 		assertEquals(20,game.score());
 	}
 	
+	@Test 
+	void test_dix_un_deux() {
+		for (int i=0 ; i<10 ; i++) {
+			game.roll(1);
+		}
+		for (int i=10 ;i <20 ; i++) {
+			game.roll(2);
+		}
+		assertEquals(30,game.score());
+	}
 	
 
 }
